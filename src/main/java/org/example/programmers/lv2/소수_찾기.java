ee6b8c2d39ec8637f45783dbd711739b7e38ca4c
Kim woo jong <skekdnwhd@gmail.com>
2024-03-04 20:52:07 +0900

    resolve : 소수 찾는 함수 변경

--- src/main/java/org/example/programmers/lv2/소수_찾기.java
+++ src/main/java/org/example/programmers/lv2/소수_찾기.java
@@ -34,7 +34,7 @@ public class 소수_찾기 {
         if (n <= 1) {
             return false;
         }
-        for (int i = 2; i * i < n; i++) {
+        for (int i = 2; i * i <= n; i++) {
             if (n % i == 0) {
                 return false;
             }
