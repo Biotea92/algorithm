e978e28fde969a0a608d6511adb7cdc6c1d79595
Kim woo jong <skekdnwhd@gmail.com>
2024-03-07 23:17:37 +0900

    solve : 메뉴 리뉴얼

--- src/main/java/org/example/programmers/lv2/메뉴_리뉴얼.java
+++ src/main/java/org/example/programmers/lv2/메뉴_리뉴얼.java
@@ -5,7 +5,7 @@ import java.util.stream.Collectors;
 
 public class 메뉴_리뉴얼 {
 
-    private static Map<String, Integer> countMap = new HashMap<>();
+    private static Map<Integer, Map<String, Integer>> countMap = new HashMap<>();
 
     public String[] solution(String[] orders, int[] course) {
         for (int count : course) {
@@ -17,23 +17,35 @@ public class 메뉴_리뉴얼 {
             }
         }
 
-        List<String> result = new ArrayList<>();
-        for (String s : countMap.keySet()) {
-            if (countMap.get(s) > 1)
-                result.add(s);
-        }
+        List<String> answer = new ArrayList<>();
 
-        String[] array = result.toArray(new String[0]);
-        Arrays.sort(array);
-        return array;
+        for (int count : countMap.keySet()) {
+            Map<String, Integer> map = countMap.get(count);
+            int max = 0;
+            for (String key : map.keySet()) {
+                max = Math.max(max, map.get(key));
+            }
+            if (max < 2)
+                continue;
+            for (String key : map.keySet()) {
+                if (map.get(key) == max) {
+                    answer.add(key);
+                }
+            }
+        }
+        return answer.stream()
+                .sorted()
+                .toArray(String[]::new);
     }
 
-    public void dfs(String[] strs, boolean[] visited, int count, Set<String> tmp, int start) {
+    private void dfs(String[] strs, boolean[] visited, int count, Set<String> tmp, int start) {
         if (tmp.size() >= count || tmp.size() >= 10) {
+            Map<String, Integer> tmpMap = countMap.getOrDefault(tmp.size(), new HashMap<>());
             String courseName = tmp.stream()
                     .sorted()
                     .collect(Collectors.joining());
-            countMap.put(courseName, countMap.getOrDefault(courseName, 0) + 1);
+            tmpMap.put(courseName, tmpMap.getOrDefault(courseName, 0) + 1);
+            countMap.put(tmp.size(), tmpMap);
             return;
         }
 
